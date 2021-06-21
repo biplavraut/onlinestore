271f2399efc68fdf18225a31805fdc295cef285d
binay8 <binayraut8@gmail.com>
2021-06-20 23:26:59 -0700

    tryThis
    db setup

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
+from app.models import Product, ProductDiscount
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductDiscount)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length = 100)
+    product_description= models.CharField(max_length = 200)
+    release_date = models.DateTimeField("date released")
+    img = models.ImageField(upload_to = "images")
+    list_price = models.FloatField(default = None, blank = True, null = True)
+    discount = models.FloatField(default=0, blank=False, null=  False)
+
+class ProductDiscount(models.Model):
+    product_id = models.ForeignKey(Product, on_delete= models.CASCADE)
+    discount_rate = models.FloatField(default=0)
+    
