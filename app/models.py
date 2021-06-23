7b602bb30a6f8ea964b62483487aa4cf5b7ee3ee
binay8 <binayraut8@gmail.com>
2021-06-22 21:09:38 -0700

    Updated models
    added on_sale boolean field

--- app/models.py
+++ app/models.py
@@ -8,10 +8,16 @@ class Product(models.Model):
     img = models.ImageField(upload_to = "images")
     list_price = models.FloatField(default = None, blank = True, null = True)
     discount = models.FloatField(default=0, blank=False, null=  False)
+    on_sale = models.BooleanField(default=False)
     def __str__(self):
         return self.product_name
+
+    def calculate_discount(self):
+        return False
+
 
 class ProductDiscount(models.Model):
     product_id = models.ForeignKey(Product, on_delete= models.CASCADE)
     discount_rate = models.FloatField(default=0)
+    
     
